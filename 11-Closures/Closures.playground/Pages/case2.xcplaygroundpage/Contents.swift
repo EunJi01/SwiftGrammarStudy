dd4ab0dd70d7f661ffe9c7985f2a141d54eabe63
EunJi <hej0102@naver.com>
2022-04-30 22:49:15 +0900

    Closures

---
+++ 11-Closures/Closures.playground/Pages/case2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+print("Start")
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 5, execute: {
+    print("End")
+})
+
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+    print("End")
+}
