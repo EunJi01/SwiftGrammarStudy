f279ff8b5dc44f31546e5f0786d47c07818adea7
EunJi <hej0102@naver.com>
2022-05-10 20:08:46 +0900

    Structures And Classes

---
+++ 16-Structures And Classes/InitializerSyntax.playground/Contents.swift
@@ -0,0 +1,59 @@
+//
+//  Copyright (c) 2018 KxCoding <kky0317@gmail.com>
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+import UIKit
+
+/*:
+ # Initializer Syntax
+ ![init](init.png)
+ */
+
+let str = "123"
+let num = Int(str)
+
+class Position {
+    var x: Double
+    var y: Double
+    
+    init() {
+        x = 0.0
+        y = 0.0
+    }
+    
+    init(value: Double) {
+        x = value
+        y = value
+    }
+}
+
+let a = Position()
+a.x
+a.y
+
+let b = Position(value: 100)
+b.x
+b.y
+
+/*:
+ ![call-init](call-init.png)
+ */
+
+
